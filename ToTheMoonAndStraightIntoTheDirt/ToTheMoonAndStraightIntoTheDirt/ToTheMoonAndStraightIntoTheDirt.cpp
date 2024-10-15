6a59968298a62099ce9684b93ffc9672a251b995
barter127 <heathercarter2006@outlook.com>
2024-10-15 02:45:05 +0100

    Added Command System

--- ToTheMoonAndStraightIntoTheDirt/ToTheMoonAndStraightIntoTheDirt/ToTheMoonAndStraightIntoTheDirt.cpp
+++ ToTheMoonAndStraightIntoTheDirt/ToTheMoonAndStraightIntoTheDirt/ToTheMoonAndStraightIntoTheDirt.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 
-long int money = 100; // needs negative values for debt
+double money = 100; // needs negative values for debt
 unsigned int assetOwned = 0;
 float assetPrice = 10;
 
-const int ASSET_MAX_AMOUNT = 1000000; // A mil
+const int ASSET_MAX_AMOUNT = 1'000'000; // A mil
 
 
 
@@ -31,22 +32,56 @@ void Sell(unsigned int amountToSell)
     money += amountToSell * assetPrice;
 }
 
+enum ListOfCommands {buy, sell};
+
+ListOfCommands Hash(std::string const& inString)
+{
+    if (inString == "buy") return buy;
+    if (inString == "sell") return sell;
+}
+
+void ReadInput()
+{
+    std::string userInput;
+    std::string command;
+    int inputAmount = 0;
+
+    std::cout << "Input command: ";
+    getline(std::cin, userInput);
+
+    // Find space
+    std::size_t spacePosition = userInput.find(" ");
+
+    // Assign start of string to space.
+    command = userInput.substr(0, spacePosition);
+
+    // Assign from space to end of string.
+    std::string amountString = userInput.substr(spacePosition, userInput.back());
+    inputAmount = std::stoi(amountString);
+
+    switch (Hash(command))
+    {
+        case buy:
+            Buy(inputAmount);
+            break;
+        case sell:
+            Sell(inputAmount);
+            break;
+    }
+        
+
+    //if (command == "buy")
+    //{
+    //    Buy(amountToBuy);
+    //}
+}
+
 int main()
 {
-    int amountToBuy = 0;
-
-    std::cout << "Input amount to buy: ";
-    std::cin >> amountToBuy;
-
-    Buy(amountToBuy);
+    ReadInput();
 
     std::cout << "Money: " << money << " Amount Owned: " << assetOwned << std::endl
         << "Input amount to sell: ";
-               
-
-    std::cin >> amountToBuy;
-
-    Sell(amountToBuy);
 
     std::cout << "Money: " << money << " Amount Owned: " << assetOwned;
 }
