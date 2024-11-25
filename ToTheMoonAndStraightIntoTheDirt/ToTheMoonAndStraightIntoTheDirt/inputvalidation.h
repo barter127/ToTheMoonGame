b5ddd37c71fd0260db7e1e9d310c7d03a71d63fe
barter127 <heathercarter2006@outlook.com>
2024-11-25 09:16:37 +0000

    Input Validation CPP

--- ToTheMoonAndStraightIntoTheDirt/ToTheMoonAndStraightIntoTheDirt/inputvalidation.h
+++ ToTheMoonAndStraightIntoTheDirt/ToTheMoonAndStraightIntoTheDirt/inputvalidation.h
@@ -1,64 +1,18 @@
 #pragma once
 
 #include <string>
-#include <iostream>
 
 // Checks each character in input string. Returns false if ANY non-digit is found.
-bool IsNumber(std::string amount)
-{
-    for (int i = 0; i < amount.length(); i++)
-    {
-        // Char found.
-        if (!isdigit(amount[i])) return false;
-    }
-
-    return true;
-}
+bool IsNumber(std::string amount);
 
 // Converts input to lowercase.
-std::string ToLowerCase(std::string inputString)
-{
-    for (std::size_t i = 0, length = inputString.length(); i < length; i++)
-    {
-        inputString[i] = tolower(inputString[i]);
-    }
-
-    return inputString;
-}
+std::string ToLowerCase(std::string inputString);
 
 /*
 Loops until a non-empty string is input.
 Returns error message if string is empty.
 */
-std::string GetValidString()
-{
-    std::string userInput;
-
-    // Get input & check string isn't empty
-    while (getline(std::cin, userInput) && userInput.empty())
-    {
-        std::cerr << "Err. Nothing Entered. Try again.\n";
-    }
-    return userInput;
-}
+std::string GetValidString();
 
 // Prolly only used to find spaces but modularity!
-int NthOccurence(std::string strToCheck, std::string lookingFor, int n)
-{
-    size_t pos = 0;
-    int iter = 0;
-
-    while (iter != n)
-    {
-        pos++;
-        pos = strToCheck.find(lookingFor, pos);
-
-        //  nth not found return failcode.
-        if (pos == std::string::npos)
-            return -1;
-
-        iter++;
-    }
-
-    return pos;
-}
+int NthOccurence(std::string strToCheck, std::string lookingFor, int n);
